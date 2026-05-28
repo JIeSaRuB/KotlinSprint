56ac60b282c8024dd2a0d87adfe60a2f770e3366
JIeSaRuB <shura.rubtsov.2002@mail.ru>
2026-05-28 20:27:58 +0300

    KS-3-5 optimized parsing

--- src/main/kotlin/lesson_3/task5.kt
+++ src/main/kotlin/lesson_3/task5.kt
@@ -2,9 +2,11 @@ package lesson_3
 
 fun main() {
     val data = "D2-D4;0"
-    val start = data.split('-')[0]
-    val finish = data.split('-')[1].split(';')[0]
-    val step = data.split(';')[1]
+    val firstParse = data.split('-')
+    val secondParse = firstParse[1].split(';')
+    val start = firstParse[0]
+    val finish = secondParse[0]
+    val step = secondParse[1]
     println(start)
     println(finish)
     println(step)
