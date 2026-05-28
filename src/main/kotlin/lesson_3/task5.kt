5e1fd1a1335c367444170595fd965831f685bc36
JIeSaRuB <shura.rubtsov.2002@mail.ru>
2026-05-28 20:32:28 +0300

    KS-3-5 optimized parsing to one object

--- src/main/kotlin/lesson_3/task5.kt
+++ src/main/kotlin/lesson_3/task5.kt
@@ -2,11 +2,10 @@ package lesson_3
 
 fun main() {
     val data = "D2-D4;0"
-    val firstParse = data.split('-')
-    val secondParse = firstParse[1].split(';')
-    val start = firstParse[0]
-    val finish = secondParse[0]
-    val step = secondParse[1]
+    val parsedData = data.split('-', ';')
+    val start = parsedData[0]
+    val finish = parsedData[1]
+    val step = parsedData[2]
     println(start)
     println(finish)
     println(step)
