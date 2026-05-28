9e7bcbff309488843fd13830c32aa5b8c390b9f4
Alexander Rubtsov <rubtsov@T8.CORP>
2026-05-28 11:57:52 +0300

    KS-3-2 delete square brackets from output

--- src/main/kotlin/lesson_3/task2.kt
+++ src/main/kotlin/lesson_3/task2.kt
@@ -5,8 +5,8 @@ fun main() {
     val firstName = "Татьяна"
     val patronymic = "Сергеевна"
     var age = 20
-    println("[$secondName $firstName $patronymic, $age]")
+    println("$secondName $firstName $patronymic, $age")
     secondName = "Сидорова"
     age = 22
-    println("[$secondName $firstName $patronymic, $age]")
+    println("$secondName $firstName $patronymic, $age")
 }
