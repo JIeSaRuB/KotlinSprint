ebc64014efa2f43592727560d3aa9af473976cb5
Alexander Rubtsov <rubtsov@T8.CORP>
2026-05-28 11:40:25 +0300

    KS-3-2 add data about Tatiana

---
+++ src/main/kotlin/lesson_3/task2.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    var secondName = "Андреева"
+    val firstName = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+    println("[$secondName $firstName $patronymic, $age]")
+    secondName = "Сидорова"
+    age = 22
+    println("[$secondName $firstName $patronymic, $age]")
+}
