b227b8adcce802c902611a826a8b5ee8be5e6be3
JIeSaRuB <shura.rubtsov.2002@mail.ru>
2026-05-30 15:43:34 +0300

    KS-4-1 fixed output format

--- src/main/kotlin/lesson_4/task1.kt
+++ src/main/kotlin/lesson_4/task1.kt
@@ -5,5 +5,5 @@ const val ALL_TABLES = 13
 fun main() {
     val tableReservationToday = 13
     val tableReservationTomorrow = 9
-    println("Доступность столиков на сегодня: ${tableReservationToday < ALL_TABLES}, Доступность столиков на завтра: ${tableReservationTomorrow < ALL_TABLES}")
+    println("[Доступность столиков на сегодня: ${tableReservationToday < ALL_TABLES}],\n[Доступность столиков на завтра: ${tableReservationTomorrow < ALL_TABLES}]")
 }
