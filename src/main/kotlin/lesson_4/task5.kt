f81453ba38a2d445322e3ff4ddfd52551e604744
Alexander Rubtsov <shura.rubtsov.2002@mail.ru>
2026-05-31 16:39:14 +0300

    KS-4-5 add some global objects

--- src/main/kotlin/lesson_4/task5.kt
+++ src/main/kotlin/lesson_4/task5.kt
@@ -1,20 +1,24 @@
 package lesson_4
 
+const val minCrew = 55
+const val maxCrew = 70
+const val minSuppliesCount = 50
+
 fun main() {
     println("Введите наличие повреждений корпуса (true/false):")
-    val hasDamage = readln().toBoolean() ?: false
+    val hasDamage = readln().toBoolean()
 
     println("Введите текущий состав экипажа:")
-    val crewSize = readln().toInt() ?: 0
+    val crewSize = readln().toInt()
 
     println("Введите количество ящиков с провизией:")
-    val suppliesCount = readln().toInt() ?: 0
+    val suppliesCount = readln().toInt()
 
     println("Введите благоприятность погоды (true/false):")
-    val isWeatherFavorable = readln().toBoolean() ?: false
+    val isWeatherFavorable = readln().toBoolean()
 
-    val canDepart = (!hasDamage && crewSize in 55..70 && suppliesCount > 50) ||
-            (hasDamage && crewSize == 70 && isWeatherFavorable && suppliesCount >= 50)
+    val canDepart = (!hasDamage && crewSize in minCrew..maxCrew && suppliesCount > minSuppliesCount) ||
+            (hasDamage && crewSize == maxCrew && isWeatherFavorable && suppliesCount >= minSuppliesCount)
 
     println(if (canDepart) "Корабль может отправиться в плавание." else "Корабль не может отправиться в плавание.")
 }
