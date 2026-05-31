9cd90ebb90d12baccfca0364710a9e897a312e62
Alexander Rubtsov <shura.rubtsov.2002@mail.ru>
2026-05-31 16:10:27 +0300

    KS-4-4 delete two objects of body parts

--- src/main/kotlin/lesson_4/task4.kt
+++ src/main/kotlin/lesson_4/task4.kt
@@ -4,15 +4,12 @@ fun main() {
     val trainingDay = 5
     val isEvenDay = trainingDay % 2 == 0
 
-    val handsAndPress = !isEvenDay
-    val backAndLegs = isEvenDay
-
     println(
         """
-        Упражнения для рук: $handsAndPress
-        Упражнения для ног: $backAndLegs
-        Упражнения для спины: $backAndLegs
-        Упражнения для пресса: $handsAndPress
+        Упражнения для рук: ${!isEvenDay}
+        Упражнения для ног: $isEvenDay
+        Упражнения для спины: $isEvenDay
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent()
     )
 }
