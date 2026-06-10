21914cad8b4f6b916d926640118fc4f4d97c9e12
Alexander Rubtsov <rubtsov@T8.CORP>
2026-06-10 14:18:32 +0300

    KS-5-1 fixed autorization message

--- src/main/kotlin/lesson_5/task1.kt
+++ src/main/kotlin/lesson_5/task1.kt
@@ -3,7 +3,7 @@ package lesson_5
 fun main() {
     val firstNumber = (1..10).random()
     val secondNumber = (1..10).random()
-    println("Введите ответ на пример: $firstNumber + $secondNumber")
+    println("Чтобы подтвердить, что Вы не бот - введите ответ на пример: $firstNumber + $secondNumber")
     val personsAnswer = readln().toInt()
 
     if (personsAnswer == firstNumber + secondNumber) println("Добро пожаловать!")
