19c04573163ffb10e15c686fd3a2b83bd1854ef2
Alexander Rubtsov <rubtsov@T8.CORP>
2026-06-25 07:38:39 +0300

    KS-5-2 fix input

--- src/main/kotlin/lesson_5/task2.kt
+++ src/main/kotlin/lesson_5/task2.kt
@@ -3,7 +3,7 @@ package lesson_5
 const val AGE_OF_MAJORITY = 18
 
 fun main() {
-    val yearOfBirthday = readln().toInt()
+    val yearOfBirthday: Int = readlnOrNull()?.toInt() ?: 0
     val currentYear = 2026
 
     if ((currentYear - yearOfBirthday) >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
