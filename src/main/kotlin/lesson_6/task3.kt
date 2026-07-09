32afe640277eba64ff84f5e7f365cb500ca115a2
Alexander Rubtsov <rubtsov@T8.CORP>
2026-07-09 15:46:15 +0300

    KS-6-2 add upgrade to timer

---
+++ src/main/kotlin/lesson_6/task3.kt
@@ -0,0 +1,12 @@
+package lesson_6
+
+fun main() {
+    print("Введите количество секунд: ")
+    var secondsCount = readln().toInt()
+    while (secondsCount > 0) {
+        println("Осталось секунд: $secondsCount")
+        Thread.sleep(1000)
+        secondsCount--
+    }
+    println("Время вышло")
+}
