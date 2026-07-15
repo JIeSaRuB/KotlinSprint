3acdabe0ed733e46cfc6160a773090162c6862e2
Alexander Rubtsov <rubtsov@T8.CORP>
2026-07-15 14:11:03 +0300

    KS-8-1 add module with summary of array items

---
+++ src/main/kotlin/lesson_8/task1.kt
@@ -0,0 +1,9 @@
+package lesson_8
+
+fun main() {
+    val viewsArray = IntArray(7) {0}
+    for (day in viewsArray.indices) {
+        viewsArray[day] = (100..10000).random()
+    }
+    println("Всего просмотров за неделю: ${viewsArray.sum()}")
+}
