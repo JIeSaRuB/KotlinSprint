d0935623c5a8086d9460efd3cec4888c886adb94
Alexander Rubtsov <rubtsov@T8.CORP>
2026-07-15 15:05:47 +0300

    KS-8-2 add module for search array item

---
+++ src/main/kotlin/lesson_8/task2.kt
@@ -0,0 +1,17 @@
+package lesson_8
+
+fun main() {
+    val ingredients = arrayOf("лапша", "сыр", "сливки", "бекон")
+    print("Введите необходимый ингредиент: ")
+    val searchItem = readln()
+    var flag = false
+    for (ingredient in ingredients) {
+        if (ingredient == searchItem) {
+            println("Игредиент $ingredient в рецепте есть")
+            flag = true
+        }
+    }
+    if (!flag) {
+        println("Такого ингредиента в рецепте нет")
+    }
+}
