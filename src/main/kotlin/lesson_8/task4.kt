388757e7e058500969705941ce8192e213ba7f20
Alexander Rubtsov <rubtsov@T8.CORP>
2026-07-16 09:02:19 +0300

    KS-8-4 fixed fatal error of case

--- src/main/kotlin/lesson_8/task4.kt
+++ src/main/kotlin/lesson_8/task4.kt
@@ -7,16 +7,15 @@ fun main() {
     }
     print("Введите ингредиент, который Вы хотели бы заменить: ")
     val toReplaceIngredient = readln()
-    var forReplaceIngredient = ""
-    if (toReplaceIngredient in ingredients) {
-        println("Введите ингредиент, на который Вы хотели бы заменить $toReplaceIngredient")
-        forReplaceIngredient = readln()
+    if (ingredients.indexOf(toReplaceIngredient) != -1) {
+        print("Введите ингредиент, на который Вы хотели бы заменить $toReplaceIngredient: ")
+        val forReplaceIngredient = readln()
+        ingredients[ingredients.indexOf(toReplaceIngredient)] = forReplaceIngredient
+        print("Готово! Вы сохранили следующий список: [")
+        for (index in (0..ingredients.size - 2)) {print("${ingredients[index]}, ")}
+        print("${ingredients[ingredients.size - 1]}]")
     }
     else {
         println("Такого ингредиента в рецепте нет")
     }
-    ingredients[ingredients.indexOf(toReplaceIngredient)] = forReplaceIngredient
-    print("Готово! Вы сохранили следующий список: [")
-    for (index in (0..ingredients.size - 2)) {print("${ingredients[index]}, ")}
-    print("${ingredients[ingredients.size - 1]}]")
 }
