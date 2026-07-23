c4dedac73bca87cba669875d36eb65d38f846b73
Alexander Rubtsov <rubtsov@T8.CORP>
2026-07-23 14:05:49 +0300

    KS-9-5 fixed input of items

--- src/main/kotlin/lesson_9/task5.kt
+++ src/main/kotlin/lesson_9/task5.kt
@@ -2,19 +2,12 @@ package lesson_9
 
 fun main() {
     val ingredientsList = mutableListOf<String>()
-    var counter = 1
-    while (counter < 6) {
+    for (counter in (1..5)) {
         print("Введите ингредиент №$counter: ")
-        val input = readln()
-        if (ingredientsList.contains(input)) {
-            continue
-        }
-        else {
-            ingredientsList.add(input)
-            counter++
-        }
+        ingredientsList.add(readln())
     }
-    ingredientsList.sort()
-    ingredientsList[0] = ingredientsList.first().replaceFirstChar { it.uppercase() }
-    println(ingredientsList.joinToString(", "))
+    val resultIngredientsList = ingredientsList.toSet().toMutableList()
+    resultIngredientsList.sort()
+    resultIngredientsList[0] = resultIngredientsList.first().replaceFirstChar { it.uppercase() }
+    print(resultIngredientsList.joinToString(", "))
 }
