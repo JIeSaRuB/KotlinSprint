56be31ff9e5c8a044c53c655d552a6b292456d41
Alexander Rubtsov <rubtsov@T8.CORP>
2026-07-28 09:38:10 +0300

    KS-10-1 fixed output stack

--- src/main/kotlin/lesson_10/task1.kt
+++ src/main/kotlin/lesson_10/task1.kt
@@ -1,13 +1,13 @@
 package lesson_10
 
-fun rollDice() : Int{
+fun rollDice(): Int {
     return (1..6).random()
 }
 
 fun main() {
     val playerDice = rollDice()
-    val computerDice = rollDice()
     println("Бросил игрок, значение: $playerDice")
+    val computerDice = rollDice()
     println("Бросил компьютер, значение: $computerDice")
     if (playerDice > computerDice) println("Победило человечество")
     else if (playerDice < computerDice) println("Победила машина")
