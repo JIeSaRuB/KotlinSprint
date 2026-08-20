6f241eba302948a131d202c0e3399efe9fe796c3
Alexander Rubtsov <rubtsov@T8.CORP>
2026-08-20 12:15:18 +0300

    KS-11-5 add builder pattern

--- src/main/kotlin/lesson_11/task5.kt
+++ src/main/kotlin/lesson_11/task5.kt
@@ -1,53 +1,90 @@
 package lesson_11
 
 class Forum() {
-    val userList = mutableListOf<ForumParticipant>()
-    val messageList = mutableListOf<ForumMessage>()
+    val userList = mutableListOf<ForumParticipant.ForumParticipantBuilder>()
+    val messageList = mutableListOf<ForumMessage.ForumMessageBuilder>()
     var nextUserId = 1
-    fun createNewUser(userName: String) {
-        val participant = ForumParticipant()
-        participant.userId = nextUserId
-        nextUserId++
-        participant.userName = userName
+    fun createNewUser(userName: String): ForumParticipant.ForumParticipantBuilder {
+        val participant = ForumParticipant.ForumParticipantBuilder()
+        participant.setUserId(nextUserId)
+        participant.setUserName(userName)
         userList.add(participant)
+        return participant
     }
+
     fun createNewMessage(userId: Int) {
         if (userId < nextUserId) {
-            val forumMessage = ForumMessage()
+            val forumMessage = ForumMessage.ForumMessageBuilder()
             forumMessage.authorId = userId
             print("Пользователь с ID = ${forumMessage.authorId}, введите сообщение: ")
             forumMessage.message = readln()
             messageList.add(forumMessage)
-        }
-        else println("Пользователя с таким ID не существует")
+        } else println("Пользователя с таким ID не существует")
     }
+
     fun printThread() {
         for (msg in messageList) {
             var printUserName = ""
             for (user in userList) {
-                if (msg.authorId == user.userId) {
+                if (msg.authorId == user.getUserId()) {
                     printUserName = user.userName
                 }
             }
-            print("$printUserName: ${msg.message} ")
+            println("$printUserName: ${msg.message} ")
         }
     }
 }
 
-class ForumParticipant() {
-    var userId = 0
-    var userName = ""
+class ForumParticipant(userId: Int, userName: String) {
+    class ForumParticipantBuilder {
+        var userId = 0
+        var userName = ""
+        fun setUserId(_userId: Int) {
+            val userId = _userId
+        }
+        fun setUserName(_userName: String) {
+            val userName = _userName
+        }
+        fun getUserId(): Int {
+            return userId
+        }
+        fun getUserName(): String {
+            return userName
+        }
+        fun build(): ForumParticipant {
+            return ForumParticipant(userId, userName)
+        }
+    }
 }
 
-class ForumMessage() {
-    var authorId = 0
-    var message = ""
+class ForumMessage(authorId: Int, message: String) {
+    class ForumMessageBuilder {
+        var authorId = 0
+        var message = ""
+        fun setAuthorId(_authorId: Int) {
+            authorId = _authorId
+        }
+        fun setMessage(_message: String) {
+            message = _message
+        }
+        fun getUserId(): Int {
+            return authorId
+        }
+        fun getMessage(): String {
+            return message
+        }
+        fun build(): ForumMessage {
+            return ForumMessage(authorId, message)
+        }
+    }
 }
 
 fun main() {
     val forum = Forum()
     forum.createNewUser("Alex")
     forum.createNewUser("SecondAlex")
+    forum.createNewMessage(1)
+    forum.createNewMessage(2)
     forum.createNewMessage(1)
     forum.createNewMessage(2)
     forum.printThread()
